4184768195668e4c568b55a0b287ff37fab709b9
Snow <polarpenguin@foxmail.com>
2019-05-03 07:28:25 +0800

    Version 0.1

---
+++ Assets/Scripts/GenerateCubes.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateCubes : MonoBehaviour
+{
+    public void CreateCubeAngle15()
+    {
+        // The script will be attached to the GameObject <Sphere>
+        // at the origin point
+        Vector3 centerPos = this.transform.position;
+        float radius = 10;
+        int angle;
+        string[] myClassmateNames = new string[24] {
+            "伍芳兰", "张敏", "张玥", "杨涵", "申波", "杨名",
+            "阙继婷", "李昌和", "薛有缘", "王位", "陈泽南", "尹红爱",
+            "曾伟康", "王帅旗", "刘伟康", "王端举", "李学锋", "阮书琪",
+            "童方超", "李亚军", "张露云", "刘湘川", "任海清", "易智隆"
+        };
+
+        // Generate a cube every 15 degrees
+        for (angle = 0; angle < 360; angle += 15)
+        {
+            // Calculate the position of the cube   
+            float x = centerPos.x + radius * Mathf.Cos(angle * 3.14f / 180f);
+            float y = centerPos.y + radius * Mathf.Sin(angle * 3.14f / 180f);
+
+            GameObject myCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            myCube.name = (angle / 15).ToString();
+            myCube.transform.position = new Vector3(x, centerPos.z, y);
+            myCube.transform.SetParent(this.transform);
+
+            Renderer render = myCube.GetComponent<Renderer>();
+            //render.material.SetColor("_Color", Color.blue);
+            render.material.SetColor("_Color", 
+                new Color(80f / 255f, 140f / 255f, 200f / 255f));
+
+            GameObject surfaceText = new GameObject();
+            surfaceText.name = "SurfaceText";
+            surfaceText.transform.position = myCube.transform.position + 
+                new Vector3(-0.01f, 0.5f, 0.0f);
+            surfaceText.transform.SetParent(myCube.transform);
+
+            TextMesh surfaceTextMesh = surfaceText.AddComponent<TextMesh>();
+            //surfaceTextMesh.text = "Test";
+            surfaceTextMesh.text = (angle / 15 + 1).ToString() +
+                "\n" + myClassmateNames[angle / 15];
+
+            surfaceTextMesh.fontSize = 100;
+            surfaceTextMesh.alignment = TextAlignment.Center;
+            surfaceTextMesh.anchor = TextAnchor.MiddleCenter;
+            surfaceTextMesh.transform.localScale = new Vector3(0.03f, 0.03f, 1f);
+            surfaceTextMesh.transform.localEulerAngles = new Vector3(90, 0, 90);
+            
+
+            // Todo: Rotate the cubes
+            //var rot = Quaternion.FromToRotation(Vector3.forward, centerPos - new Vector3(x, centerPos.z, y));
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateCubeAngle15();
+        //float radius = 20f;
+        //for (int i = 1; i <= 24; i++)
+        //{
+        //    float angle = i * Mathf.PI * 2f / 24;
+        //    Vector3 newPos = new Vector3(Mathf.Cos(angle) * radius, 0, Mathf.Sin(angle) * radius);
+        //    GameObject go = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), newPos, Quaternion.identity);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/RetateAroundCenter.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetateAroundCenter : MonoBehaviour
+{
+    private bool isSpinning;
+    private float[] cubesAngles;
+    private float startAngle;
+    private float finalAngle;
+    private float currentLerpRotationTime;
+
+    private int playedTimes;
+    private Material lastTimeSelectedCubeMaterial;
+
+    public GameObject centerSphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.isSpinning = false;
+
+        // Fill the necessary angles
+        this.cubesAngles = new float[] {
+            0, 15, 30, 45, 60, 75, 90,
+            105, 120, 135, 150, 165, 180,
+            195, 210, 225, 240, 255, 270,
+            285, 300, 315, 330, 345 };
+
+        this.startAngle = 0;
+        this.finalAngle = 0;
+        this.currentLerpRotationTime = 0;
+        this.playedTimes = 0;
+        this.lastTimeSelectedCubeMaterial = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.RotateAround(centerCube.transform.position, 
+        //    new Vector3(0.0f,1.0f,0.0f), 100*Time.deltaTime);
+
+        if (false == this.isSpinning)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                this.StartRotation();
+                this.isSpinning = true;
+                if (0 != this.playedTimes && null != this.lastTimeSelectedCubeMaterial)
+                {
+                    this.lastTimeSelectedCubeMaterial.SetColor("_Color",
+                new Color(80f / 255f, 140f / 255f, 200f / 255f));
+                }
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        float maxLerpRotationTime = 4f;
+        // Increment timer once per frame
+        this.currentLerpRotationTime += Time.deltaTime;
+        if(this.currentLerpRotationTime > maxLerpRotationTime || 
+            this.centerSphere.transform.eulerAngles.z == this.finalAngle)
+        {
+            // Stop spinning
+            this.currentLerpRotationTime = maxLerpRotationTime;
+            this.isSpinning = false;
+            this.startAngle = this.finalAngle % 360;
+            this.PickClassmateByAngle();
+            this.playedTimes++;
+        }
+
+        // Calculate current position using linear interpolation
+        float t = this.currentLerpRotationTime / maxLerpRotationTime;
+        // This formulae allows to speed up at start and speed down at the end of rotation.
+        // Try to change this values to customize the speed
+        t = t * t * t * (t * (6f * t - 15f) + 10f);
+
+        float currentRotationAngle = Mathf.Lerp(this.startAngle, this.finalAngle, t);
+        this.centerSphere.transform.eulerAngles = new Vector3(0, currentRotationAngle, 0);
+        //Debug.Log(this.startAngle);
+        //Debug.Log(this.finalAngle);
+    }
+
+    private void StartRotation()
+    {
+        this.currentLerpRotationTime = 0f;
+
+        int fullRotationTimes = 5;
+        float randomLastTimeRotationAngle = this.cubesAngles[
+                UnityEngine.Random.Range(0, this.cubesAngles.Length)
+            ];
+
+        this.finalAngle = -(360 * fullRotationTimes + randomLastTimeRotationAngle);
+        this.isSpinning = true;
+    }
+
+    void PickClassmateByAngle()
+    {
+        string[] myClassmateNames = new string[24] {
+            "伍芳兰", "张敏", "张玥", "杨涵", "申波", "杨名",
+            "阙继婷", "李昌和", "薛有缘", "王位", "陈泽南", "尹红爱",
+            "曾伟康", "王帅旗", "刘伟康", "王端举", "李学锋", "阮书琪",
+            "童方超", "李亚军", "张露云", "刘湘川", "任海清", "易智隆"
+        };
+
+        int whichClassmate = (Mathf.RoundToInt(this.startAngle) + 360) % 360 / 15;
+        GameObject classmateCube = this.transform.Find(
+            whichClassmate.ToString()).gameObject;
+
+        Renderer classmateCubeRender = classmateCube.GetComponent<Renderer>();
+        this.lastTimeSelectedCubeMaterial = classmateCubeRender.material;
+        classmateCubeRender.material.SetColor("_Color",
+                new Color(255f / 255f, 120f / 255f, 255f / 255f));
+    }
+}
